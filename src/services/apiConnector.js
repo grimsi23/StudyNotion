4d5de3bec2c53e7310e6f78b8129617c80ca92ce
grimsi23 <mehtagrimsi@gmail.com>
2025-08-30 00:02:28 +0530

    Fix apiConnector and update BASE_URL env

--- src/services/apiConnector.js
+++ src/services/apiConnector.js
@@ -4,12 +4,13 @@ export const axiosInstance = axios.create({});
 
 export const apiConnector = (method, url, bodyData, headers, params) => {
   console.log("ENV BASE_URL:", process.env.REACT_APP_BASE_URL);
-  
+
   return axiosInstance({
     method: `${method}`,
     url: `${url}`,
     data: bodyData ? bodyData : null,
     headers: headers ? headers : null,
     params: params ? params : null,
+    withCredentials: true, 
   });
 };
