54a5285b440eaffb2d329d094e3a048062b562aa
grimsi23 <mehtagrimsi@gmail.com>
2025-09-28 21:30:24 +0530

    render issue

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json({ limit: '10mb'}));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: ["http://localhost:3000"],
+  origin: "http://localhost:3000",
   credentials: true
 };
 app.use(cors(corsOptions));
